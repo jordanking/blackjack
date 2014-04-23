02ca5025162ddde288694ac028cb6e019326405d
arpost71 <arp68@georgetown.edu>
2014-04-23 11:47:54 -0400

    stats and settings

--- src/blackjack/SettingsPanel.java
+++ src/blackjack/SettingsPanel.java
@@ -3,9 +3,14 @@
  */
 package blackjack;
 
+import java.awt.GridLayout;
 import java.awt.LayoutManager;
+import java.awt.event.KeyEvent;
 
+import javax.swing.JComponent;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
 
 /**
  * @author Alex Post
@@ -20,29 +25,65 @@ public class SettingsPanel extends JPanel {
 		// TODO Auto-generated constructor stub
 	}
 
-	/**
-	 * @param layout
-	 */
-	public SettingsPanel(LayoutManager layout) {
-		super(layout);
-		// TODO Auto-generated constructor stub
+//	/**
+//	 * @param layout
+//	 */
+//	public SettingsPanel(LayoutManager layout) {
+//		super(layout);
+//		// TODO Auto-generated constructor stub
+//	}
+//
+//	/**
+//	 * @param isDoubleBuffered
+//	 */
+//	public SettingsPanel(boolean isDoubleBuffered) {
+//		super(isDoubleBuffered);
+//		// TODO Auto-generated constructor stub
+//	}
+//
+//	/**
+//	 * @param layout
+//	 * @param isDoubleBuffered
+//	 */
+//	public SettingsPanel(LayoutManager layout, boolean isDoubleBuffered) {
+//		super(layout, isDoubleBuffered);
+//		// TODO Auto-generated constructor stub
+//	}
+	public void init(){
+		
 	}
-
-	/**
-	 * @param isDoubleBuffered
-	 */
-	public SettingsPanel(boolean isDoubleBuffered) {
-		super(isDoubleBuffered);
-		// TODO Auto-generated constructor stub
-	}
-
-	/**
-	 * @param layout
-	 * @param isDoubleBuffered
-	 */
-	public SettingsPanel(LayoutManager layout, boolean isDoubleBuffered) {
-		super(layout, isDoubleBuffered);
-		// TODO Auto-generated constructor stub
-	}
-
+	
+	public void tabbedPane() {
+        //super(new GridLayout(1, 1));
+        
+        JTabbedPane tabbedPane = new JTabbedPane();
+        
+        JComponent panel1 = makeTextPanel("Panel #1");
+        tabbedPane.addTab("Manual", null, panel1,
+                "Manual Play");
+        tabbedPane.setMnemonicAt(0, KeyEvent.VK_1);
+        
+        JComponent panel2 = makeTextPanel("Panel #2");
+        tabbedPane.addTab("Automatic", null, panel2,
+                "Simulation Settings");
+        tabbedPane.setMnemonicAt(1, KeyEvent.VK_2);
+        
+        
+        
+        //Add the tabbed pane to this panel.
+        add(tabbedPane);
+        
+        //The following line enables to use scrolling tabs.
+        tabbedPane.setTabLayoutPolicy(JTabbedPane.SCROLL_TAB_LAYOUT);
+    }
+	
+	 protected JComponent makeTextPanel(String text) {
+	        JPanel panel = new JPanel(false);
+	        JLabel filler = new JLabel(text);
+	        filler.setHorizontalAlignment(JLabel.CENTER);
+	        panel.setLayout(new GridLayout(1, 1));
+	        panel.add(filler);
+	        return panel;
+	    }
+	
 }
--- src/blackjack/StatsPanel.java
+++ src/blackjack/StatsPanel.java
@@ -3,16 +3,25 @@
  */
 package blackjack;
 
-import java.awt.LayoutManager;
+import java.awt.BorderLayout;
+import java.awt.Button;
+import java.awt.Font;
+import java.awt.Panel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-import javax.swing.JPanel;
+import javax.swing.JTextArea;
 
 /**
  * @author Alex Post
  *
  */
-public class StatsPanel extends JPanel {
+public class StatsPanel extends BPanel implements ActionListener {
 
+	Button backButton;
+	JTextArea statistics, title;
+	Integer totalWins, cashWon, totalLosses, cashLost, avgHold, totalHold;
+	
 	/**
 	 * 
 	 */
@@ -20,29 +29,79 @@ public class StatsPanel extends JPanel {
 		// TODO Auto-generated constructor stub
 	}
 
-	/**
-	 * @param layout
-	 */
-	public StatsPanel(LayoutManager layout) {
-		super(layout);
-		// TODO Auto-generated constructor stub
-	}
+//	/**
+//	 * @param layout
+//	 */
+//	public StatsPanel(LayoutManager layout) {
+//		super(layout);
+//		// TODO Auto-generated constructor stub
+//	}
+//
+//	/**
+//	 * @param isDoubleBuffered
+//	 */
+//	public StatsPanel(boolean isDoubleBuffered) {
+//		super(isDoubleBuffered);
+//		// TODO Auto-generated constructor stub
+//	}
+//
+//	/**
+//	 * @param layout
+//	 * @param isDoubleBuffered
+//	 */
+//	public StatsPanel(LayoutManager layout, boolean isDoubleBuffered) {
+//		super(layout, isDoubleBuffered);
+//		// TODO Auto-generated constructor stub
+//	}
 
-	/**
-	 * @param isDoubleBuffered
-	 */
-	public StatsPanel(boolean isDoubleBuffered) {
-		super(isDoubleBuffered);
-		// TODO Auto-generated constructor stub
+	public void init(){
+		title = new JTextArea();
+		statistics = new JTextArea();
+		backButton = new Button("Back");
+		Panel statsPanel = new Panel();
+		
+		title.setFont(new Font("Comic Sans", Font.BOLD, 24));
+		
+		backButton.addActionListener(this);
+		
+		statsPanel.setLayout(new BorderLayout(100, 50));
+		title.setEditable(false);
+		statistics.setEditable(false);
+		
+		calculateValues();
+		
+		title.setText("Statistics:");
+		statistics.setText("Total Wins: " + totalWins + "\nCash Won: " + cashWon + "\nTotal Losses: " + totalLosses +
+				"\nCash Lost: " + cashLost + "\nAverage Hold Value: " + avgHold);
+		
+		statsPanel.add(statistics, BorderLayout.LINE_START);
+		statsPanel.add(backButton, BorderLayout.PAGE_END);
+		statsPanel.add(title, BorderLayout.PAGE_START);
+		
+		add(statsPanel);
 	}
-
-	/**
-	 * @param layout
-	 * @param isDoubleBuffered
+	
+	/*
+	 * This class will eventually get values from the gameboard class.  The methods I have here are made up, but the general
+	 * idea is there.
 	 */
-	public StatsPanel(LayoutManager layout, boolean isDoubleBuffered) {
-		super(layout, isDoubleBuffered);
-		// TODO Auto-generated constructor stub
+	
+	public void calculateValues(){
+//		for (int i = 0; i < holdNumber.size(); i++){
+//			totalHold += holdNumber.get(i);
+//		}
+//		avgHold = totalHold/holdNumber.size();
+//		totalWins = board.getWins();
+//		totalLosses = board.getLosses();
+//		cashWon = board.getCashWon();
+//		cashLost = board.getCashLost();
+		
+	}
+	
+	public void ActionPerformed(ActionEvent event){
+		if(event.getSource() == backButton){
+			panelManager.actionPerformed(new ActionEvent(this, BlackjackApplet.REMOVE, "blackjack.StatsPanel"));
+		}
 	}
 
 }
