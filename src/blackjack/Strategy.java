0e4f3dc1838218a4671d6234146e54f1aea91278
amm359 <amm359@georgetown.edu>
2014-04-26 18:00:20 -0400

    Updated Panels and Strategy Class

--- src/blackjack/Strategy.java
+++ src/blackjack/Strategy.java
@@ -5,6 +5,7 @@ package blackjack;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Map;
 
 /**
  * @author alyssamiller
@@ -14,58 +15,22 @@ import java.util.Collections;
  */
 public class Strategy {
 
-	private int totalPlayerHand; // total card value of player's hand
-	private Card firstPlayerCard; // first card player is dealt
-	private Card secondPlayerCard; // second card player is dealt
-	private Card dealerFaceUpCard; // value of dealer's card that is face up
+	private Map<String, Map<Integer, GameAction>> strategyTable; //combinations of player and dealer hands
 	
 	private GameAction desiredAction; // the action to take with the dealt hand 
 	
-	
 	/**
-	 * Constructor. Sets totalPlayerHand and desiredGameAction
-	 * @param totalPlayerHand
-	 * @param desiredAction
-	 * @param dealerFaceUpCard
+	 * Default Constructor. Sets the optimal strategy.
 	 */
-	public Strategy(Card firstPlayerCard, Card secondPlayerCard, Card dealerFaceUpCard, GameAction desiredAction) {
-		//total the number of points the player has and set totalPlayerHand to that amount
-		this.setTotalPlayerHand(firstPlayerCard.getCardRank().getCardPoints() + secondPlayerCard.getCardRank().getCardPoints());
+	public Strategy() {
 		
-		this.setFirstPlayerCard(firstPlayerCard);
-		this.setSecondPlayerCard(secondPlayerCard);
-		this.setDealerFaceUpCard(dealerFaceUpCard);
-		this.setDesiredAction(desiredAction);
+		
+		
+		
+		
 	}
 
-	/**
-	 * @return the totalPlayerHand
-	 */
-	public int getTotalPlayerHand() {
-		return totalPlayerHand;
-	}
-
-	/**
-	 * @param totalPlayerHand the totalPlayerHand to set
-	 */
-	public void setTotalPlayerHand(int totalPlayerHand) {
-		this.totalPlayerHand = totalPlayerHand;
-	}
-
-	/**
-	 * @return the dealerFaceUpCard
-	 */
-	public Card getDealerFaceUpCard() {
-		return dealerFaceUpCard;
-	}
-
-	/**
-	 * @param dealerFaceUpCard the dealerFaceUpCard to set
-	 */
-	public void setDealerFaceUpCard(Card dealerFaceUpCard) {
-		this.dealerFaceUpCard = dealerFaceUpCard;
-	}
-
+	
 	/**
 	 * @return the desiredAction
 	 */
@@ -80,31 +45,19 @@ public class Strategy {
 		this.desiredAction = desiredAction;
 	}
 
-	/**
-	 * @return the firstPlayerCard
-	 */
-	public Card getFirstPlayerCard() {
-		return firstPlayerCard;
-	}
 
 	/**
-	 * @param firstPlayerCard the firstPlayerCard to set
+	 * @return the strategyTable
 	 */
-	public void setFirstPlayerCard(Card firstPlayerCard) {
-		this.firstPlayerCard = firstPlayerCard;
+	public Map<String, Map<Integer, GameAction>> getStrategyTable() {
+		return strategyTable;
 	}
 
-	/**
-	 * @return the secondPlayerCard
-	 */
-	public Card getSecondPlayerCard() {
-		return secondPlayerCard;
-	}
 
 	/**
-	 * @param secondPlayerCard the secondPlayerCard to set
+	 * @param strategyTable the strategyTable to set
 	 */
-	public void setSecondPlayerCard(Card secondPlayerCard) {
-		this.secondPlayerCard = secondPlayerCard;
+	public void setStrategyTable(Map<String, Map<Integer, GameAction>> strategyTable) {
+		this.strategyTable = strategyTable;
 	}
 }
