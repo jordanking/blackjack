38ed11d1f3bf58acf38b9ecb6e3f26ba14b8850d
amm359 <amm359@georgetown.edu>
2014-04-28 15:57:50 -0400

    created Junit class and package

---
+++ src/jUnitTests/StrategyTest.java
@@ -0,0 +1,67 @@
+/**
+ * 
+ */
+package jUnitTests;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author alyssamiller
+ *
+ */
+public class StrategyTest {
+
+	public StrategyTest() {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link blackjack.Strategy#getGameActionForHands(java.lang.String, java.lang.Integer)}.
+	 */
+	@Test
+	public void testGetGameActionForHands() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link blackjack.Strategy#setGameActionForHands(java.lang.String, java.lang.Integer, blackjack.GameAction)}.
+	 */
+	@Test
+	public void testSetGameActionForHands() {
+		fail("Not yet implemented");
+	}
+
+}
