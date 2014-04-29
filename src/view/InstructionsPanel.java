a72f0739f450862cdbedd1ae8a77aeb44fd5572e
amm359 <amm359@georgetown.edu>
2014-04-29 17:11:18 -0400

    Finished InstructionsPanel

--- src/view/InstructionsPanel.java
+++ src/view/InstructionsPanel.java
@@ -37,8 +37,7 @@ public class InstructionsPanel extends BPanel implements ActionListener{
 	public void init() {
 
 		
-		// set the size of this panel
-		//setPreferredSize(new Dimension(800, 800));
+		// set the size of this panel\
 		setPreferredSize(new Dimension(1000, 800));
 		
 		setLayout(new BorderLayout());
@@ -64,34 +63,12 @@ public class InstructionsPanel extends BPanel implements ActionListener{
 		background.setVisible(true);
 		add(instructionsPanel, BorderLayout.CENTER);
 		
-		/*
-		instructionsPanel.add(appInstructions = new JTextArea(), BorderLayout.CENTER);
-
-		
-		appInstructions.setEditable(false);
-		appInstructions.setText("Blackjack: HOW TO PLAY\n\n" +
-				"The goal of Blackjack is to beat the dealer's cards, which" +
-				"\ncan be done in one of three ways:\n\n" +
-				"- Having a total of 21 on the first two cards dealt to the player \n" +
-				"- Reach a final score higher than the dealer without exceeding 21 \n" +
-				"- Allow the dealer to draw additional cards, \"Hit,\" until the hand exceeds 21 \n\n" +
-				"VALUE OF CARDS:\n\n" +
-				"The value of a numerical card is that of its integer value, the value of \n" +
-				"a face card is 10, and an Ace is either 1 or 11.\n\n"
-				+ "BLACKJACK PLAYING OPTIONS \n\n "
-				+ "SPLITTING PAIRS: When you have two cards of the same denomination, you may \n"
-				+ "split he pairs into two separate hands and play each hand individually. You \n"
-				+ "must wager the same amount as your original bet on each split hand. If you \n"
-				+ "have split Aces, you get only one \"hit\" on each Ace.\n\n"
-				+ "DOUBLING DOWN: Consider this if you think you can win with only one additional \n"
-				+ "card. You may wager additional money up to your original bet, and receive one \n"
-				+ "more card. A player has the option of doubling down after a hand has been split.");
-		
-		add(instructionsPanel, BorderLayout.NORTH);
-		*/
 	}
 	
 	/** 
+	 * initializeInputButtons()
+	 * 
+	 * initializes buttons
 	 * @return panel the panel for the buttons
 	 * @throws HeadlessException
 	 * @since 1.0
@@ -115,6 +92,11 @@ public class InstructionsPanel extends BPanel implements ActionListener{
 		
 	}
 	
+	/**
+	 * actionPerformed()
+	 * Listens for the click events for each button and adds/removes the correct
+	 * panel accordingly, or exits
+	 */
 	public void actionPerformed(ActionEvent event) {
 		if (event.getSource() == exitButton) {
 			System.exit(0);
